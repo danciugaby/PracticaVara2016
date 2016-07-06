542ceb12755c0d6b9396b0620d2bc593fb0ab87c
gabriel danciu <gabriel.danciu@dynamicventures.com>
2016-07-06 15:34:38 +0300

    a new Push on Git with a new C# solution

---
+++ Day2/Chapter3/Chapter3/Panda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter3
+{
+    public class Panda
+    {
+        public Panda Mate;
+        public void Marry(Panda partner)
+        {
+            Mate = partner;
+            partner.Mate = this;
+        }
+    }
+}
---
+++ Day2/Chapter3/Chapter3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Panda panda = new Panda();
+            Panda she = new Panda();
+            panda.Mate = she;
+        }
+    }
+}
