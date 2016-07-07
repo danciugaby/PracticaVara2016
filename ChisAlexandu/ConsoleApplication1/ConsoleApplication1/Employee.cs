d3bda66a9c160d912b952744f9ed41a91c71c2b9
Chis Alexandru <Alexutu1994@gmail.com>
2016-07-07 16:27:06 +0300

    Alexandru second attempt to commit

---
+++ ChisAlexandu/ConsoleApplication1/ConsoleApplication1/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChisAlexandruEmployee
+{
+    class Employee : Person
+    {
+        #region Functions
+        public Employee(string FirstName, string LastName, int EmployeeNumber, int Salary, bool Hired) : base(FirstName, LastName)
+        {
+            mEmployeeNumber = EmployeeNumber;
+            mSalary = Salary;
+            mHired = Hired;
+        }
+        #region MemberFunctions
+        public void promote(int inRaiseAmount = 1000)
+        {
+            mSalary += inRaiseAmount;
+        }
+        public void demote(int inDemeritAmount = 1000)
+        {
+            mSalary -= inDemeritAmount;
+        }
+        public void hire()
+        {
+            mHired = true;
+        }
+        public void fire()
+        {
+            mHired = false;
+        }
+        public override void display()
+        {
+            Console.Write("First name: " + mFirstName + " Last name: " + mLastName + " NumberID: " + mEmployeeNumber + " Salary: " + mSalary + " Hired " + mHired + "\r\n");
+        }
+        #endregion
+        #endregion
+        #region Members
+        private int mEmployeeNumber;
+        private int mSalary;
+        private bool mHired;
+        #endregion
+    }
+}
---
+++ ChisAlexandu/ConsoleApplication1/ConsoleApplication1/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChisAlexandruEmployee
+{
+    class Person
+    {
+        public Person(string FirstName, string LastName)
+        {
+            mFirstName = FirstName;
+            mLastName = LastName;
+        }
+        public virtual void display()
+        {
+            Console.Write(mFirstName + " " + mLastName);
+        }
+        #region Members
+        protected string mFirstName;
+        protected string mLastName;
+        #endregion
+    }
+}
