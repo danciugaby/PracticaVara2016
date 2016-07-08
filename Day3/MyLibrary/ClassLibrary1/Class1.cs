5edae4190985cfae40e8c223b4aba9a28eb0b19f
gabriel danciu <gabriel.danciu@dynamicventures.com>
2016-07-08 12:07:51 +0300

    ClassLibrary(dll) Unit Test

---
+++ Day3/MyLibrary/ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public void f(string val)
+        {
+            Console.WriteLine(val);
+        }
+    }
+}
