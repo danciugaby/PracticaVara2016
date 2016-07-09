5302ea54b7fbf3c621a8aa380e72f7cdceda11b8
gabriel danciu <gabriel.danciu@dynamicventures.com>
2016-07-09 20:49:01 +0300

    morequeries

---
+++ Day3/Chapter4/MoreDelegatesAndEvents/StandardEventPattern.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreDelegatesAndEvents
+{
+    public class PriceChangedEventArgs : System.EventArgs
+    {
+        public readonly decimal LastPrice;
+        public readonly decimal NewPrice;
+        //the handler
+        public PriceChangedEventArgs(decimal lastPrice, decimal newPrice)
+        {
+            LastPrice = lastPrice;
+            NewPrice = newPrice;
+        }
+
+    }
+
+    public class Stock
+    {
+        string symbol;
+        decimal price;
+        public Stock(string symbol) { this.symbol = symbol; }
+
+        public event EventHandler<PriceChangedEventArgs> PriceChanged;
+
+        protected virtual void OnPriceChanged(PriceChangedEventArgs e)
+        {
+            PriceChanged.Invoke(this, e);
+        }
+
+        public decimal Price
+        {
+            get { return price; }
+            set
+            {
+                if (price == value) return;
+                decimal oldPrice = price;
+                price = value;
+                //the raiser
+                //raise the event pointing exacly at the desired handler
+                OnPriceChanged(new PriceChangedEventArgs(oldPrice, price));
+            }
+        }
+    }
+}
---
+++ Day3/Chapter4/UnitTestProjectAdvanced/UnitTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using LambdaExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoreDelegatesAndEvents;
+
+namespace UnitTestProjectAdvanced
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestGenericDelegates()
+        {
+            MoreDelegatesAndEvents.Point[] vals = new Point[3];
+            vals[0] = new Point();
+            vals[0].X = 1;
+            vals[1] = new Point();
+            vals[1].Y = 1;
+            vals[2] = new Point();
+            //its like having an interface without having to plug the 
+            //"interface" physic connection
+           GenericDelegate<CallerForDelegate>.Transform(vals, CallerForDelegate.ATransformer);
+
+        }
+
+        static void stock_PriceChanged(object sender, PriceChangedEventArgs e)
+        {
+            if ((e.NewPrice - e.LastPrice) / e.LastPrice > 0.1M)
+                Console.WriteLine("Alert, 10% stock price increase!");
+        }
+
+        [TestMethod]
+        public void TestEvents()
+        {
+            Stock stock = new Stock("THPW");
+            try
+            {
+                stock.Price = 27.10M;
+            }
+            catch (Exception ex)
+            {
+             Console.WriteLine(ex.Message);   
+            }
+           
+            // Register with the PriceChanged event
+            //the register
+            stock.PriceChanged += stock_PriceChanged;
+            stock.Price = 31.59M;
+        }
+        
+        [TestMethod]
+        public void TestLambdaExpressions()
+        {
+            LEinDelegates.ExampleLE();
+            LEinDelegates.QueryOperator();
+            LEinDelegates.MoreQueries();
+        }
+    }
+    
+}
