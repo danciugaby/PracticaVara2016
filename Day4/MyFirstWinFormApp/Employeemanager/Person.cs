d963937f2805fb80fdacd6e7ecb0f1f50c1e2563
gabriel danciu <gabriel.danciu@dynamicventures.com>
2016-07-11 15:31:32 +0300

    data bindings

---
+++ Day4/MyFirstWinFormApp/Employeemanager/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeemanager
+{
+    public class Person
+    {
+        public Person(string name, string fname)
+        {
+            Name = name;
+            Firstname = fname;
+        }
+
+        public string Name { get; set; }
+        public string Firstname { get; set; }
+    }
+}
