afe0accc186e3bdfe60290936f6dd4f2066b30c4
gabriel danciu <gabriel.danciu@dynamicventures.com>
2016-07-13 16:38:17 +0300

    Enumerations

---
+++ Day6/Linq/Linq/Linqer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+
+    public class Linqer
+    {
+        public static void TestEnum()
+        {
+            AnEnumerable anenumarable = new AnEnumerable();
+            anenumarable.PopulateWithDymmy();
+            int c = Count(anenumarable);
+        }
+
+        public static int Count(IEnumerable e)
+        {
+            int count = 0;
+            foreach (object element in e)
+            {
+                var subCollection = element as IEnumerable;
+                if (subCollection != null)
+                    count += Count(subCollection);
+                else
+                    count++;
+            }
+            return count;
+        }
+    }
+}
